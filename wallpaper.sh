1086f39888664089ee8d6db60a9c5b159e43892c
Sebastian Krohn <seb@gaia.sunn.de>
2014-08-13 16:03:08 +0200

    Wallpaper.sh is now reacting to display config and resolution changes

--- wallpaper.sh
+++ wallpaper.sh
@@ -3,48 +3,89 @@
 # fancy background stuff
 
 (
-      dir="$HOME/.wallpaper"
-      ctime=0
-      y=0
+   set -o allexport
+   set -o nounset
 
-      while [ -d "$dir" ]; do
+   dir="$HOME/.wallpaper"
+   ctime=0
+   delay="30" # minutes
+   y=0
+
+   # to clean up subprocesses in case of sigint
+   # (for debugging etc)
+   function handle_sigint()
+   {
+      for proc in $(jobs -p) ; do 
+	 kill $proc
+      done
+   }
+   trap handle_sigint SIGINT
+
+   function set_background () {
+      set -o errexit
+
+      test -f "$1"
+
+      # make a copy of the wallpaper for the lockscreen
+      # i3lock -i /tmp/.wallpaper.png -t
+      convert "$1" \
+		-resize "$(xrandr | awk '/*/{print $1; exit}')^" \
+		-gravity center -crop "$(xrandr | awk '/*/{print $1; exit}')+0+0" \
+		+repage /tmp/.wallpaper.png &
+
+      if [ "$(hostname)" == "adminwks06" ] ; then
+		convert "$1" "$1" \
+	    +append -quality 85 /tmp/.wallpaper.png
+		feh --bg-fill /tmp/.wallpaper.png
+      else
+		feh --bg-fill "$1"
+      fi
+   }
+
+   # this monitors the display configuration for a change and triggers a reload
+   # of the background in case the active configuration changes
+   (
+      displayconf=""
+      sleep 5 # wait for the trap to be set
+
+      while [ 1 ] ; do
+		if [ "$(xrandr -q | grep '*' | md5sum | cut -f 1 -d ' ')" != "$displayconf" ] ; then
+			kill -s ALRM $$
+			displayconf="$(xrandr -q | grep '*' | md5sum | cut -f 1 -d ' ')"
+		fi
+		sleep 10
+      done
+   ) &
+
+   while [ -d "$dir" ] ; do
       # update file list if directory has changed
       if [ $ctime != $(stat -c '%Z' $dir) ] ; then
-         ctime=$(stat -c '%Z' $dir)
-         range=`find "$dir" -type f | wc -l`
-         IFS='
+		ctime=$(stat -c '%Z' $dir)
+		range=`find "$dir" -type f | wc -l`
+		IFS='
 '
-         wallpapers=($(find "$dir" -type f))
-         unset IFS
+		wallpapers=($(find "$dir" -type f))
+		unset IFS
       fi
 
       let "number = $RANDOM % ($range-1)"
 
       # avoid displaying the same picture again
       if [ $number = $y ] ; then
-         let "number = ($number + 1) % ($range-1)"
+		let "number = ($number + 1) % ($range-1)"
       fi
-
       y=$number
 
-      # make a copy of the wallpaper for the lockscreen
-      # i3lock -i /tmp/.wallpaper.png -t
-      convert "${wallpapers[$number]}" \
-	-resize "$(xrandr | awk '/*/{print $1; exit}')^" \
-	-gravity center -crop "$(xrandr | awk '/*/{print $1; exit}')+0+0" \
-	+repage /tmp/.wallpaper.png &
+      set_background "${wallpapers[$number]}"
+      trap "set_background \"${wallpapers[$number]}\"" SIGALRM
 
-      # reset chosen wallpaper in decreasing intervalls to avoid
-      # awesome wallpaper interference at startup
-      for t in 30s 90s 28m ; do
-         if [ "$(hostname)" == "adminwks06" ] ; then
-            convert "${wallpapers[$number]}" "${wallpapers[$number]}" \
-	      +append -quality 85 /tmp/.combined.jpg
-            feh --bg-fill /tmp/.combined.jpg
-         else
-            feh --bg-fill "${wallpapers[$number]}"
-         fi
-         sleep $t
+      # "If bash is waiting for a command to complete and receives a signal for
+      #  which a trap has been set, the trap will not be executed until the command
+      #  completes"
+      for i in {1..$delay} ; do #minutes
+	 for j in {1..6} ; do # 10 seconds
+	    sleep 10
+	 done
       done
    done
 ) &
